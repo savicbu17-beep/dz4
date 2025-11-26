cc334d439928d6c73cc4f58b2fdcd5c9fa075d5a
User <savic.bu17@gmail.com>
2025-11-26 15:02:33 +0200

    commit

--- main.py
+++ main.py
@@ -4,24 +4,22 @@ class Person:
         self.age = age
 
     def info(self):
-        return f"Ім'я: {self.name}, Вік: {self.age}"
+        return f"Ім'я {self.name} Вік {self.age}"
 
 
 class Driver(Person):
     def __init__(self, name, age, license_number):
-        # Викликаємо конструктор батьківського класу
         super().__init__(name, age)
         self.license_number = license_number
 
     def info(self):
-        # Розширюємо метод батьківського класу
         base_info = super().info()
-        return f"{base_info}, Посвідчення водія: {self.license_number}"
+        return f"{base_info} Посвідчення водія {self.license_number}"
 
 
 # Перевірка роботи
 person = Person("Олег", 28)
-driver = Driver("Максим", 35, "AB987654")
+driver = Driver("Максім", 35, "ВХ2342АС")
 
 print(person.info())
 print(driver.info())
